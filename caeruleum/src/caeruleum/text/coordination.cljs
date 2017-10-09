dd554874d6a02e346324008fc3228c6ab37aa261
cedretaber <cedretaber@gmail.com>
2017-10-09 23:37:34 +0900

    Skip explains.

--- caeruleum/src/caeruleum/text/coordination.cljs
+++ caeruleum/src/caeruleum/text/coordination.cljs
@@ -1,7 +1,18 @@
 (ns caeruleum.text.coordination)
 
+(def bar "-------------------------------------------------------")
+
 (defn coordinate [[row-title row-author & rest]]
   (let [title [:h1.title {:key :title} row-title]
         author [:h3.author {:key :author} row-author]
-        texts (map-indexed (fn [idx txt] [:p.book {:key idx} txt]) rest)]
+        texts (loop [line-cnt 0
+                     ruby-cnt 0
+                     skipped false
+                     acm []
+                     [line & rest] rest]
+                (cond
+                  (nil? rest) (reverse acm)
+                  (= line bar) (recur line-cnt ruby-cnt (not skipped) acm rest)
+                  skipped (recur line-cnt ruby-cnt skipped acm rest)
+                  :else (recur (+ line-cnt 1) ruby-cnt skipped (cons [:p.book {:key line-cnt} line] acm) rest)))]
     (concat [title author] texts)))
